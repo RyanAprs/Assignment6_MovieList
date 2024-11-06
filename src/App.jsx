c44697a44e1c910a01a9d777bfa59091d65f2828
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-11-06 23:40:27 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Movie />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Movie.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movie = () => {
+  return <div className="text-red-500">Movie</div>;
+};
+
+export default Movie;
